1738aae3f5690f81a52bb7c8b4d5450d41bd252d
meghabhatiya <46623252+meghabhatiya@users.noreply.github.com>
2019-01-12 16:27:43 +0530

    Add files via upload
    12 jan 2019

---
+++ first.py
@@ -0,0 +1,19 @@
+a=7
+b=2
+c=a+b
+print ("Addition is:",c)
+
+c=a-b
+print ("subtraction is:",c)
+c=a*b
+print ("multiplication is:",c)
+c=a/b
+print ("division is:",c)
+c=a%b
+print ("modulo is",c)
+
+c=a**b
+print("exponential is",c)
+
+c=a//b
+print("quotient is",c)
---
+++ second.py
@@ -0,0 +1,2 @@
+a=input ("Enter the no.")
+print(a)
