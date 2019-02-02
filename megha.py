51874d2eabd419b003df2e95c6a4e8a104be31da
Megha bhatiya <46623252+meghabhatiya@users.noreply.github.com>
2019-02-02 15:41:32 +0530

    Add files via upload

---
+++ megha.py
@@ -0,0 +1,11 @@
+a=int(input("what is the side of square\n"))
+r=int(input("enter radius of circle\n"))
+l=int(input("enter the length of rectangle\n"))
+b=int(input("enter the breadth of rectangle\n"))
+
+b=a**2
+d=3.14*(r**2)
+c=l*b
+print ("Area of square with side {} is {}".format(a,b))
+print("Area of radius of circle {} is {}".format(a,b))
+print("Area of rectangle with length{} and breadth{}".format(a,b))
---
+++ screaming.py
@@ -0,0 +1,5 @@
+a=input("what you want to scream\n")
+b=int(input("How many times you want to scream\n"))
+
+for i in range(b):
+	print(f"{a} \n")
